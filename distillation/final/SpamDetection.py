3dcaf14c164ea056b1d791c70e0b98ce8076165d
Himangini Agrawal <himangini@Himanginis-MacBook-Pro.local>
2018-12-10 19:45:47 -0800

    Spam Detection Function

--- distillation/final/SpamDetection.py
+++ distillation/final/SpamDetection.py
@@ -4,7 +4,7 @@
 # ## Spam Detection Factor
 # #### This notebook is a subset of Spam_detection_and_Stance_detection.ipynb
 
-# In[1]:
+# In[10]:
 
 
 import numpy as np
@@ -14,7 +14,9 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-# In[2]:
+# #### Load the liar liar dataset
+
+# In[11]:
 
 
 data = pd.read_csv('../himangini_train.tsv', sep='\t')
@@ -23,7 +25,9 @@ data_text['index'] = data_text.index
 documents = data_text
 
 
-# In[3]:
+# #### Using SMS Spam Detection Kaggle dataset
+
+# In[12]:
 
 
 spam_dataset = pd.read_csv("../spam.csv", encoding = "latin-1")
@@ -31,7 +35,7 @@ spam_dataset = spam_dataset[['v1', 'v2']]
 spam_dataset = spam_dataset.rename(columns = {'v1': 'label', 'v2': 'text'})
 
 
-# In[4]:
+# In[13]:
 
 
 from nltk.corpus import stopwords
@@ -56,14 +60,18 @@ def alternative_review_messages(msg):
     return msg
 
 
-# In[5]:
+# #### Splitting the model into test and train 
+
+# In[14]:
 
 
 spam_dataset['text'] = spam_dataset['text'].apply(review_messages)
 X_train, X_test, y_train, y_test = train_test_split(spam_dataset['text'], spam_dataset['label'], test_size = 0.1, random_state = 1)
 
 
-# In[6]:
+# #### Using Support Vector Machine to build a spam/ham classifier model
+
+# In[15]:
 
 
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -74,17 +82,32 @@ svm = svm.SVC(C=1000)
 svm.fit(X_train, y_train)
 
 
-# In[7]:
+# #### Using the trained model on liar liar dataset, the below python function will check any news and return ham or spam as per the classification
+
+# In[16]:
 
 
 def spam_checker(msg):
     msg = vectorizer.transform([msg])
     prediction = svm.predict(msg)
     return prediction[0]
-#spam_checker("Says the Annies List political group supports third-trimester abortions on demand.")
 
 
-# In[8]:
+# #### Now, lets have a python function which can give us the numerical value for alternus vera computation
+
+# In[17]:
+
+
+ham1 = "ham"
+def spam_detection_checker(msg):
+    spam_value = spam_checker(msg)
+    if spam_value == ham1:
+        return 0
+    else:
+        return 1
+
+
+# In[18]:
 
 
 class SpamDetection:
